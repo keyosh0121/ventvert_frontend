43e35fcfae54d4c3bccfbca86c07654ef94a5aa8
Kei Yoshiyama <32564487+keyosh0121@users.noreply.github.com>
2021-01-12 14:58:49 +0900

    Update axios.js

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -9,6 +9,7 @@ import Store from '@/store/index.js'
 // axios.defaults.baseURL = process.env.baseURL || process.env.apiUrl || '';
 // axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
 // axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+console.log(process.env.BACKEND_BASE_URL);
 axios.defaults.baseURL = process.env.BACKEND_BASE_URL;
 
 
