f2e8b368a2d5e60b6cebdc1f9a4503a29b47ebce
System Administrator <root@yoshiyamakeinoMacBook-Pro.local>
2021-01-12 21:49:28 +0900

    Use Environment Variable

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -9,7 +9,8 @@ import Store from '@/store/index.js'
 // axios.defaults.baseURL = process.env.baseURL || process.env.apiUrl || '';
 // axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
 // axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
-axios.defaults.baseURL = process.env.BACKEND_BASE_URL;
+axios.defaults.baseURL = process.env.VUE_APP_BACKEND_BASE_URL;
+
 
 
 let config = {
